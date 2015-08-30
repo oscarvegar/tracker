eee0a69c44452681d1f10a807e535d4a5aa0b32f
oscar.vega <oscar.vega@atomicware.mx>
2015-08-30 01:40:30 -0500

    fullscreen map

--- tracker-core/config/routes.js
+++ tracker-core/config/routes.js
@@ -34,6 +34,12 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
+  },
+  '/fullscreen':{
+    view:'403',
+    locals: {
+      layout: 'fullscreen'
+    }
   }
 
   /***************************************************************************
