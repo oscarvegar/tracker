2a03b12ca384f42103c6678d809f2a98452b33de
oscar monroy <oscar.monroyg@gmail.com>
2015-09-02 00:09:06 -0500

    cambio en solicitudes

--- tracker-core/assets/js/module/solicitud.js
+++ tracker-core/assets/js/module/solicitud.js
@@ -1,5 +1,5 @@
 var app = angular.module( "track-solicitud", ['ui.router'] );
-app.controller( "SolicitudCtrl", function($scope, $http, $rootScope, $location) {
+app.controller( "SolicitudCtrl", function($scope, $http, $rootScope, $location, $log) {
 
 	$rootScope.tituloPagina = "Crear Solicitud";
     $rootScope.iconoPagina = "icon-edit";
@@ -23,7 +23,8 @@ app.controller( "SolicitudCtrl", function($scope, $http, $rootScope, $location) 
         if (!itemActual) {
             $scope.lista.push({
                 Producto: p,
-                Cantidad: 1
+                Cantidad: 1,
+                Unidad:p.Unidad
             });
         } else {
             itemActual.Cantidad++;
@@ -37,16 +38,44 @@ app.controller( "SolicitudCtrl", function($scope, $http, $rootScope, $location) 
     };
     
 
-    $scope.productos = [{"Id": "1",  "Producto": "Cemento Apasco Toneladas",  "Imagen": "http://materialeslavena.com/ventas/images/SACO%20APASCO.JPG"},
-{"Id": "2",  "Producto": "Varilla 1/2 de pulgada Toneladas",  "Imagen": "http://www.apsaceros.com/1956/img/varilla_1.jpg"},
-{"Id": "3",  "Producto": "Varilla de 1/4 de pulgada Toneladas",  "Imagen": "http://www.apsaceros.com/1956/img/varilla_1.jpg"},
-{"Id": "4",  "Producto": "Cal Toneladas",  "Imagen": "http://construoreza.com.mx/images/MATERIAL%20CONSTRUCCION/CAL.png"},
-{"Id": "6",  "Producto": "Tabique rojo Millar",  "Imagen": "http://www.constructodo.com.mx/upload/productos/acero-estructural/tabique-rojo.jpg"},
-{"Id": "7",  "Producto": "Block Millar", "Imagen": "http://docs.engineeringtoolbox.com/documents/1731/concrete_masonry_block.png"},
-{"Id": "8",  "Producto": "Arena Camiones",  "Imagen": "http://static.imujer.com/sites/default/files/lasmanualidades/como-preparar-masa-de-arena.jpg"},
-{"Id": "9",  "Producto": "Alambron Toneladas",  "Imagen": "http://www.cofiasa.com.mx/wp-content/uploads/2014/09/alambron-e1411752694974.jpg"}
+    $scope.productos = [{"Id": "1010001", "Producto":  "ALAMBRE RECOCIDO CAL. 16", "Unidad": "KG"},
+{"Id": "1010002",  "Producto": "ALAMBRON DE 1/4", "Unidad": "KG"},
+{"Id": "1010003",  "Producto": "ALAMBRE GALVANIZADO CAL. 16", "Unidad": "KG"},
+{"Id": "1010004",  "Producto": "ANGULO DE ACERO 1 1/2' X 1 1/2' DE 1/4", "Unidad" : "KG"},
+{"Id": "1010005",  "Producto": "ANGULO DE ACERO 3/4 X 3/4 DE 1/8", "Unidad" : "KG"},
+{"Id": "1010006",  "Producto": "ANGULO DE ACERO 1 X  DE 1/8", "Unidad": "KG"},
+{"Id": "1010007",  "Producto": "ANGULO DE ACERO 1 X 1 DE 3/16", "Unidad": "KG"},
+{"Id": "1010008",  "Producto": "ANGULO DE ACERO 1 1/4 X 1 1/4 DE1/4", "Unidad": "KG"},
+{"Id": "1020001",  "Producto": "VARILLA CORRUGADA DE 3/8 No, 3", "Unidad": "KG"},
+{"Id": "1020002",  "Producto": "VARILLA CORRUGADA DE 1/2 No. 4", "Unidad": "KG"},
+{"Id": "1020003",  "Producto": "VARILLA CORRUGADA DE 5/8 No. 5", "Unidad": "KG"},
+{"Id": "1020004",  "Producto": "VARILLA CORRUGADA DE 3/4 No. 6", "Unidad": "KG"},
+{"Id": "1020005",  "Producto": "VARILLA CORRUGADA DE 1 No 8", "Unidad": "KG"},
+{"Id": "1020006",  "Producto": "VARILLA CORRUGADA DE 1 1/4 No. 10", "Unidad": "KG"},
+{"Id": "1030001",  "Producto": "ADITIVO MEYCO SA 160", "Unidad": "LT"},
+{"Id": "1030002",  "Producto": "ADITIVO EXPANSOR Y PLASTIFICANTE", "Unidad": "LT"},
+{"Id": "1030003",  "Producto": "ADHESIVO HIT RE 500/330", "Unidad": "PZ"},
+{"Id": "1030004",  "Producto": "ADHESIVO PARA JUNTA PREFORMADA", "Unidad": "PZ"},
+{"Id": "1030005",  "Producto": "ADITIVO PARA FRAGUADO DE CONCRETO", "Unidad": "LT"},
+{"Id": "1030006",  "Producto": "ADITIVO P/ CONCRETO INCORPORADOR DE AIRE", "Unidad": "LT"}
 
 ]
+
+   $scope.totalItems = 170;
+    $scope.currentPage = 1;
+
+      
+    $scope.setPage = function (pageNo) {
+      $scope.currentPage = pageNo;
+    };
+
+    $scope.pageChanged = function() {
+      $log.log('Page changed to: ' + $scope.currentPage);
+    };
+
+      $scope.maxSize = 5;
+      $scope.bigTotalItems = 10;
+      $scope.bigCurrentPage = 1;
 
   
 });
