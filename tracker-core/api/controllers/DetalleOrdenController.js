f5dbbd8a72e1fffe5d1be65e75dfa2d48302c46d
oscar monroy <oscar.monroyg@gmail.com>
2015-09-15 14:45:16 -0500

    cambios en detalle orden

--- tracker-core/api/controllers/DetalleOrdenController.js
+++ tracker-core/api/controllers/DetalleOrdenController.js
@@ -5,15 +5,45 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var Q = require("q"); 
+
 module.exports = {
 
 	 'getDetalle/:orderId':function(req,res){
 	 	var params = req.allParams();
 	 	var ordenId = params.orderid;
 	 	console.log(ordenId);
-	 	DetalleOrden.find().where({orden: ordenId}).populateAll().then(function(dataAll){
-	 		console.log(dataAll);
-	  		res.json(dataAll);
+	 	DetalleOrden.find().where({orden: ordenId}).populate('orden').then(function(dataAll){
+	 		if(dataAll){
+	 			if(dataAll.length>0){
+	 				var result = []
+	 					for(var i=0; i<dataAll.length; i++){
+	 						console.log("valor id orden");
+	 						console.log(dataAll[i].orden.id);
+	 						result.push(
+								Orden.find().where({id: dataAll[i].orden.id}).populateAll()
+	 						);
+	 						
+	 					}
+							Q.all(result)
+							.allSettled(result).then(function(detalleordenes){
+								console.log("Valor mergeado");
+								console.log(detalleordenes);
+								res.json(detalleordenes);
+
+							}).catch(function(err,err2){
+							      console.log(err)
+								console.log(err2)
+							});
+
+	 			}else{
+			          return res.json(dataAll);
+			        }
+	 		}else{
+			          return res.json(dataAll);
+			        }
+	 		/*console.log(dataAll);
+	  		res.json(dataAll);*/
 	 	});
 
 	 }
